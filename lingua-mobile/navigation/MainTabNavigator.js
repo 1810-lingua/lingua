6ea8dadb6118387ada4b707944473e85c6eafdfe
Alex Kramer <alexkrameris@gmail.com>
2019-01-25 15:30:19 -0500

    react-native app has been started, basic views labelled.

---
+++ lingua-mobile/navigation/MainTabNavigator.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { Platform } from "react-native";
+import {
+  createStackNavigator,
+  createBottomTabNavigator
+} from "react-navigation";
+
+import TabBarIcon from "../components/TabBarIcon";
+import HomeScreen from "../screens/HomeScreen";
+import FlashCardScreen from "../screens/FlashCardScreen";
+import AllWordsScreen from "../screens/AllWordsScreen";
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: "Home",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === "ios"
+          ? `ios-information-circle${focused ? "" : "-outline"}`
+          : "md-information-circle"
+      }
+    />
+  )
+};
+
+const FlashCardScreenStack = createStackNavigator({
+  FlashCards: FlashCardScreen
+});
+
+FlashCardScreenStack.navigationOptions = {
+  tabBarLabel: "Flash Cards",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-link" : "md-link"}
+    />
+  )
+};
+
+const AllWordsStack = createStackNavigator({
+  AllWords: AllWordsScreen
+});
+
+AllWordsStack.navigationOptions = {
+  tabBarLabel: "All Words",
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === "ios" ? "ios-options" : "md-options"}
+    />
+  )
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  FlashCardScreenStack,
+  AllWordsStack
+});
