4417ce451ef0779235ba44671900dc84ede79452
Sahil Pabby <pabs124@gmail.com>
2019-01-31 17:04:08 -0500

    styled popup and made one form for login and signup

--- eventPage.js
+++ eventPage.js
@@ -4,10 +4,11 @@ const menuItem = {
   contexts: ["selection"]
 };
 
-const translate = async (word) => {
+const translate = async word => {
   const apiKey =
     "trnsl.1.1.20190126T211920Z.755eef10f6ae2719.f84530139cfd895621304f2b1ed34456a340ebb6";
-  const baseUrl = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
+  const baseUrl =
+    "https://translate.yandex.net/api/v1.5/tr.json/translate?key=";
   const text = "&text=";
   const lang = "&lang=es";
   const fullUrl = baseUrl + apiKey + lang + text + word;
@@ -19,16 +20,18 @@ const translate = async (word) => {
   } catch (err) {
     console.log("Error: " + err);
   }
-}
+};
 
 chrome.contextMenus.create(menuItem);
 
-chrome.contextMenus.onClicked.addListener(async (clickData) => {
+chrome.contextMenus.onClicked.addListener(async clickData => {
   const selection = clickData.selectionText;
   if (clickData.menuItemId == "Lingua" && selection) {
     const translatedSelection = await translate(selection);
-    chrome.storage.sync.get(["userid"], async (user) => {
-      const newRef = firebase.database().ref(`/${user.userid}/${selection}/`);
+    chrome.storage.sync.get(["userid"], async user => {
+      const newRef = firebase
+        .database()
+        .ref(`/${user.userid}/spanish/${selection}/`);
       await newRef.set({
         word: selection,
         translation: translatedSelection[0],
@@ -36,7 +39,7 @@ chrome.contextMenus.onClicked.addListener(async (clickData) => {
       });
     });
 
-    chrome.storage.sync.get(["total", "words"], (items) => {
+    chrome.storage.sync.get(["total", "words"], items => {
       if (!items.words) {
         items.words = [];
       }
